b592c0ae97c4016ff63b6aa5e78c5526fbdbad7a
Monika <monika@monika-HP-G62-Notebook-PC.(none)>
2014-03-09 23:38:07 +0100

    tablica

--- zad1/doc/html/search/functions_64.js
+++ zad1/doc/html/search/functions_64.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['dodaj',['dodaj',['../classtab.html#ae6e506c6264acf4367f6927624e3fed0',1,'tab::dodaj(int e)'],['../classtab.html#a90dc8cfffe8244a6ae1218fd0444f399',1,'tab::dodaj(tab B)']]]
+  ['dodaj',['dodaj',['../classtab.html#ae6e506c6264acf4367f6927624e3fed0',1,'tab']]]
 ];
--- zad1/doc/html/search/functions_6f.js
+++ zad1/doc/html/search/functions_6f.js
@@ -1,7 +1,5 @@
 var searchData=
 [
   ['odwroc',['odwroc',['../classtab.html#ac618d6a3d5e17cd6afb0ba5c225f4416',1,'tab']]],
-  ['operator_3d',['operator=',['../classtab.html#a05351f5dbd8a13885d98b3165245e498',1,'tab']]],
-  ['operator_3d_3d',['operator==',['../classtab.html#a82eda0243a55cc93e412c46c1eef1b49',1,'tab']]],
-  ['operator_5b_5d',['operator[]',['../classtab.html#a3c549759a4a4870aa49c798c05915e2b',1,'tab::operator[](int ind)'],['../classtab.html#aa0611573459a2dad5500d8315ef6fccc',1,'tab::operator[](int ind) const ']]]
+  ['operator_3d',['operator=',['../classtab.html#a05351f5dbd8a13885d98b3165245e498',1,'tab']]]
 ];
